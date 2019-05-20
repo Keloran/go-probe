cd0997f2ee4b2b197a34a28b6fa31aab864995aa
Max Hooton <max.hooton@auden.co.uk>
2019-05-20 14:47:02 +0100

    tests

--- probe_test.go
+++ probe_test.go
@@ -1,8 +1,13 @@
-package probe
+package probe_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/keloran/go-probe"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -23,5 +28,28 @@ func TestProbe(t *testing.T) {
 		resp, err := probe.Probe()
 		assert.Equal(t, test.err, err)
 		assert.Equal(t, test.expect, resp)
+	}
+}
+
+func TestHTTP(t *testing.T) {
+	tests := []struct{
+		expect probe.Healthy
+	}{
+		{
+			expect: probe.Healthy{
+				Status: "pass",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		request, _ := http.NewRequest("GET", "/", bytes.NewBuffer([]byte{}))
+		response := httptest.NewRecorder()
+		probe.HTTP(response, request)
+		assert.Equal(t, 200, response.Code)
+		body, _ := ioutil.ReadAll(response.Body)
+		healthy := probe.Healthy{}
+		_ = json.Unmarshal(body, &healthy)
+		assert.Equal(t, test.expect, healthy)
 	}
 }
